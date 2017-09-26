40c1d11b9fcccd91db1bb013f3a7f9de548b5239
Bob <bobevans547@gmail.com>
2017-09-26 09:58:25 -0400

    added SimpleQuiz

--- JS/SimpleQuiz.js
+++ JS/SimpleQuiz.js
@@ -30,7 +30,7 @@ function submitAnswers(){
 		score++;
 		}
 	}
-	//Display results   alert won't workas well
+	//Display results   alert won't work as well
 	var results = document.getElementById('results');
 	results.innerHTML== '<h3> You scored <span>' +score+ '</span> out of <span>'+total+'</span></h3>';
 	alert('You scored' +score+' out of '+total);
